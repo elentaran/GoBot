b5da46925ce058810a58885b1336c2ea40c1159b
Arpad Rimmel <Arpad.Rimmel@airpx.com>
2012-07-26 10:57:45 +0200

    adding a gitignore file and the gtest lib

--- bitboard/main.cpp
+++ bitboard/main.cpp
@@ -1,13 +1,21 @@
 // GoBot
 
 #include "GTPengine.h"
+#include "gtest/gtest.h"
+using namespace std;
+
+
 
 
 int main(int nb_args, char* args[]) {
 
-  srand ( time(NULL) );
-  GTPengine global_GTPengine =  GTPengine(nb_args,args);
-  global_GTPengine.launch();
+    cout << "hello world" << endl;
+    testing::InitGoogleTest(&nb_args, args);
+    return RUN_ALL_TESTS();
 
-  return 1;
+    srand ( time(NULL) );
+    GTPengine global_GTPengine =  GTPengine(nb_args,args);
+    global_GTPengine.launch();
+
+    return 1;
 }
